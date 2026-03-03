726814bb5d86616c65180f8633d1194c4c307ad8
Your Name <you@example.com>
2026-03-03 17:55:21 +0500

    c

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.contact_view,name = 'contact'),
-    path("succes/", views.contact_success,name='contact_success')
+    path("succes/", views.contact_success,name='contact_success'),
+    path("list/", views.contactlist, name="contactlist"),
+    path("<int:pk>/", views.contact_detail, name="contact_detail")
 ]
--- contact_us/views.py
+++ contact_us/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import ContactForm
+from .models import ContactMessage
+
 def contact_view(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
@@ -9,6 +11,16 @@ def contact_view(request):
     else:
         form=ContactForm()
     return render(request, "contact_us/contact.html", {"form": form})
+
+def contactlist(request):
+    contacts = ContactMessage.objects.all()
+    return render(request, 'contact_us/contactlist.html', {'contacts': contacts})
+
+
 def contact_success(request):
     return render(request,'contact_us/contact_success.html')
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(ContactMessage, pk=pk)
+    return render(request, 'contact_us/contactdetail.html', {'contact': contact})
 # Create your views here.
