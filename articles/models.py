be446e226816c7304b622925f46efc1b6cf70846
baha <baqytbekov.baqbergen@gmail.com>
2026-03-03 17:56:16 +0500

    patching_patch

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib import admin
 
+class Author(models.Model):
+    text = models.CharField(max_length =200)
+    def __str__(self):
+        return self.name
+
+
 class Articles(models.Model):
     title = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
 
     def __str__(self):
             return self.title
